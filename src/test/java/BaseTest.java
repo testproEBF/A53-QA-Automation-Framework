ac2ba3d15afaf0e1343a8701549ea5cf79d56194
testproEBF <enrile.fuentes@testpro.io>
2024-01-24 22:12:23 -0500

    Update Homework26

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,11 +5,10 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
@@ -20,6 +19,7 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.time.Duration;
 import java.util.HashMap;
+import java.util.Map;
 
 public class BaseTest {
 
@@ -56,7 +56,6 @@ public class BaseTest {
     private WebDriver pickBrowser (String browser) throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
         String gridURL = "http://192.168.1.222:4444";
-
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*");
 
@@ -86,49 +85,63 @@ public class BaseTest {
             case "grid-chrome":
                 caps.setCapability("browserName", "chrome");
                 return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
-            case "cloud":
-                return lamdaTest();
+            case "cloud-chrome":
+                return lambdaTest("chrome");
+            case "cloud-firefox":
+                return lambdaTest("firefox");
             default:
                 WebDriverManager.chromedriver().setup();
                 return driver = new ChromeDriver(chromeOptions);
         }
     }
 
-    public WebDriver lamdaTest() throws MalformedURLException {
-        String hubURL = "@hub.lamdatest.com/";
-        String username = System.getenv("LT_USERNAME") == null ? "" : System.getenv("LT_USERNAME");
-        String authkey = System.getenv("LT_ACCESS_KEY") == null ? "" : System.getenv("LT_ACCESS_KEY");
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-
-        ChromeOptions browserOptions = new ChromeOptions();
-        browserOptions.setPlatformName("macOS Sonoma");
-        browserOptions.setBrowserVersion("120.0");
+    public WebDriver lambdaTest(String browser) throws MalformedURLException{
+        WebDriver driver = null;
         HashMap<String, Object> ltOptions = new HashMap<String, Object>();
-        ltOptions.put("username", "enrile.fuentes");
-        ltOptions.put("accessKey", "12K3XhCWuHpuTAhIxeeFb0bXUbWgYtBXNwY4dWpnrczOY5F3SZ");
         ltOptions.put("visual", true);
         ltOptions.put("video", true);
         ltOptions.put("resolution", "2560x1440");
-        ltOptions.put("project", "Untitled");
-        ltOptions.put("name", "Homework25ParallelTesting");
-        ltOptions.put("selenium_version", "4.5.0");
-        ltOptions.put("driver_version", "120.0");
-        ltOptions.put("w3c", true);
-        ltOptions.put("plugin", "java-testNG");
-        browserOptions.setCapability("LT:Options", ltOptions);
+        ltOptions.put("build", "Homework25 Cloud Parallel Testing");
 
-        URL lambdaUrl;
+        String username = System.getenv("LT_USERNAME") == null ? "" : System.getenv("LT_USERNAME");
+        String authkey = System.getenv("LT_ACCESS_KEY") == null ? "" : System.getenv("LT_ACCESS_KEY");
+
+        String hubURL = "@hub.lambdatest.com/wd/hub";
+        URL lambdaUrl = null;
         try{
             lambdaUrl = new URI("https://" + username + ":" + authkey + hubURL).toURL();
         }catch (URISyntaxException e) {
             lambdaUrl = new URL("https://" + username + ":" + authkey + hubURL);
         }
-        System.out.println("url: " + lambdaUrl.toString());
-        Assert.assertNotNull(lambdaUrl, "url: " + lambdaUrl.toString());
-        return new RemoteWebDriver(lambdaUrl, capabilities);
 
+        switch (browser){
+
+
+            case "firefox":
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.setPlatformName("macOS Sonoma");
+                firefoxOptions.setBrowserVersion("121.0");
+                ltOptions.put("name", "Handling on Firefox");
+                firefoxOptions.setAcceptInsecureCerts(true);
+                firefoxOptions.setCapability("LT:Options", ltOptions);
+
+                driver = new RemoteWebDriver(lambdaUrl, firefoxOptions);
+                break;
+            case "chrome":
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.setPlatformName("macOS Sonoma");
+                chromeOptions.setBrowserVersion("120.0");
+                ltOptions.put("name", "Handling on Chrome");
+                chromeOptions.setAcceptInsecureCerts(true);
+                chromeOptions.setCapability("LT:Options", ltOptions);
+
+                driver = new RemoteWebDriver(lambdaUrl, chromeOptions);
+
+                break;
+        }
+
+
+        return driver;
     }
 
     @AfterMethod
