22a08633595e29ee48eae237dc22a89fce794bcf
testproEBF <enrile.fuentes@testpro.io>
2024-03-01 18:47:58 -0500

    Added files in resources

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -4,29 +4,37 @@ import io.cucumber.testng.TestNGCucumberRunner;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
+@Test
 @CucumberOptions(
         features = {"src/test/resources/features/Login.feature"
 //                , "src/test/resources/features/CreateSmartPlaylist.feature"
                   , "src/test/resources/features/Logout.feature"
-        }
+        },
+        publish = true
 )
 
 public class CucumberRunner extends AbstractTestNGCucumberTests {
     private TestNGCucumberRunner testNGCucumberRunner;
 
+    // This method is executed before the class starts running and initializes the TestNGCucumberRunner.
     @BeforeClass(alwaysRun = true)
-    public void setUpCucumber(){
+    public void setUpCucumber() {
         testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
     }
 
-    @DataProvider
-    public Object[][] features(){
-        return testNGCucumberRunner.provideScenarios();
+    // This method overrides the scenarios() method from the AbstractTestNGCucumberTests class.
+    // It provides the scenarios (feature files) to be executed by the TestNG framework.
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
     }
 
+    // This method is executed after the class finishes running and performs cleanup tasks.
     @AfterClass(alwaysRun = true)
-    public void tearDownClass(){
+    public void tearDownClass() {
         testNGCucumberRunner.finish();
     }
 }
